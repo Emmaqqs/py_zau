8445f118fd288c0a6276df4680bedce429dcf5a8
emma-Flo-KSH <emmaksh10@gmail.com>
2026-03-26 14:21:06 -0600

    hola

--- combobox.py
+++ combobox.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 ventana = tk.Tk()
 combo = ttk.Combobox(ventana, values=["México",
-"USA", "Canadá"])
+"USA", "Canadá","Chalco"])
 combo.pack()
 combo.current(0)
 ventana.mainloop()
